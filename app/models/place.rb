898d608bbcfe30ba0105c1d9906ebec172b52ae5
Alfonso <ajzuniga1@miuandes.cl>
2018-03-27 12:41:11 -0300

    Santiago's part

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,10 @@
 class Place < ApplicationRecord
   has_many :events
+
+  def last_attendance()
+    last_event = Event.where(place:self).order("start_date").limit(1).first
+    number_tickets = ticket.where(event: last_event).count
+    return number_tickets
+  end
+  
 end
